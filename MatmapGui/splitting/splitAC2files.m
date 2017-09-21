2e41dc92d3703aae9b490dfa4d277dd26a0ea760
AntonRodenhauser <anton.rodenhauser@hotmail.com>
2017-09-21 17:24:15 -0600

    file splitting even more memory efficient now

--- MatmapGui/splitting/splitAC2files.m
+++ MatmapGui/splitting/splitAC2files.m
@@ -1,6 +1,5 @@
 function splitAC2files(handle)
 % callback to 'Split Files'
-
 %%%% get all necessary inputs. If you plan to use this function outside of matmap, this (and 'frames', see getFrames function) is all you need to change:
 
 global myScriptData
@@ -10,7 +9,7 @@ allInputFiles=myScriptData.ACQFILENAME; %cell array of all files that are conver
 calfile=myScriptData.CALIBRATIONFILE;  % path to .cal8 file
 DO_CALIBRATION=myScriptData.CALIBRATE_SPLIT;  % do you want to calibrate files as you convert them into .mat files?
 idx2beSplitted=myScriptData.FILES2SPLIT;  % indices of the files in allInputFiles, that will be splitted..
-
+intervalLength=myScriptData.SAMPLEFREQ*myScriptData.SPLITINTERVAL;
 
 %%%% start here, first check input:
 if isempty(outputdir)
@@ -23,7 +22,6 @@ elseif isempty(myScriptData.SPLITINTERVAL)
     errordlg('The length of a splitted file was not given.')
     return
 end
-
 
 
 %%%% set up stuff
@@ -47,123 +45,93 @@ for p=1:length(allInputFiles)
     end
     cd(olddir);
     
-    
-    
-    
     if isgraphics(h), waitbar(p/length(allInputFiles),h), end
     
-    
-    %%%% split if file is to be splitted
-    splitted = false;
-    if ismember(p, idx2beSplitted)
-        splitted = true;
-        %%%% determine where you want to split file
-        frames=getFrames(TSindex);
+  
+    %%%% split the file
+    if ismember(p, idx2beSplitted)  % if file should be splitted
+        %%%% determine where file is to be splitted;
+        nFrames = TS{TSindex}.numframes;
+        nSplitIntervals = floor(nFrames/intervalLength);  % file will be splitted into nSplitIntervals + 1 files
         
-        %%%% split TSindex into newTsIndices
-        newTSindices=splitUnprocTS(TSindex, frames);
-        
-        %%%% add origin to newTSindices
-        for q=1:length(newTSindices)
-            TS{newTSindices(q)}.origin=[TS{newTSindices(q)}.filename '_' sprintf('%03d',q)];
-        end
-    else
-        TS{TSindex}.origin = TS{TSindex}.filename;
-    end
-    
-    
-    %%%% now save the file
-    
-    if splitted
-        for spltTSIdx = newTSindices
-            ts=TS{spltTSIdx};
-            TS{spltTSIdx} = 1;  % make it empty
-            ts.filename = ['Run' sprintf('%04d',fileCount) '.mat'];
-            fname=fullfile(outputdir,ts.filename);
-           
-            %get ts_info
+        for interval = 1:nSplitIntervals
+            %%%% set up the new ts structure
+            fn=fieldnames(TS{TSindex});
+            for q=1:length(fn)
+                if strcmp(fn{q},'potvals'), continue, end
+                ts.(fn{q})=TS{TSindex}.(fn{q});
+            end   
+            ts.numframes = intervalLength;
+            ts.origin = [TS{TSindex}.filename '_' sprintf('%03d',interval)];
+            ts.filename = ['Run' sprintf('%04d',fileCount) '.mat'];  % the new filename          
+            fileCount = fileCount + 1; 
+            
+            
+            %%%%% put potvals in new ts and delte them in old ts
+            ts.potvals = TS{TSindex}.potvals(:,1:intervalLength);  
+            TS{TSindex}.potvals(:,1:intervalLength) = [];
+            
+            
+            %%%% get ts_info
             fn=fieldnames(ts);
             for q=1:length(fn)
                 if strcmp(fn{q},'potvals'), continue, end
                 ts_info.(fn{q})=ts.(fn{q});
             end   
-
+            
+            
+            %%%% save the new ts and ts_info and clear them   
+            fname=fullfile(outputdir,ts.filename);
             save(fname,'ts','ts_info','-v6')
-            clear ts ts_info
-            fileCount = fileCount + 1;
         end
-    else
+        
+        %%%%% now also save the last part of old ts (which has length < intervalLength)
+        nRemindingFrames = size(TS{TSindex}.potvals,2);
+        
+        if nRemindingFrames > 300  % if there are enough reminding frames so it makes sense to save them..
+            ts.potvals =  TS{TSindex}.potvals;
+            
+            ts.filename = ['Run' sprintf('%04d',fileCount) '.mat'];  % the new filename          
+            fileCount = fileCount + 1;
+            ts_info.filename = ts.filename;
+            
+            ts.origin = [TS{TSindex}.filename '_' sprintf('%03d',interval)];
+            ts_info.origin = ts.origin;
+            
+            TS{TSindex} = 1; % free memory
+        
+            %%%% save the new ts and ts_info and clear them   
+            fname=fullfile(outputdir,ts.filename);
+            save(fname,'ts','ts_info','-v6')
+        end
+    else  % if file is not to be splitted;
         ts=TS{TSindex};
+        ts.origin =  ts.filename;
         TS{TSindex} = 1;
         ts.filename = ['Run' sprintf('%04d',fileCount) '.mat'];
-        fname=fullfile(outputdir,ts.filename);
+        fileCount = fileCount + 1;
+        
 
         %get ts_info
         fn=fieldnames(ts);
         for q=1:length(fn)
             if strcmp(fn{q},'potvals'), continue, end
             ts_info.(fn{q})=ts.(fn{q});
-        end   
-
+        end
+        
+        fname=fullfile(outputdir,ts.filename);
         save(fname,'ts','ts_info','-v6')
-        clear ts ts_info 
-        fileCount = fileCount + 1;
-    end  
+        clear ts ts_info
+    end
+    if isgraphics(h), waitbar(p/length(allInputFiles),h), end
 end
-
+clear global TS
+clear ts ts_info
 if isgraphics(handle), delete(handle), end
 if isgraphics(h), delete(h), end
 
 
-
-function newTSindices = splitUnprocTS(TSindex,frames)
-%splits TS{TSindex} in length(channels) new ts and saves the new ts in
-%newTSindices. Clears the old TS.
-%input:
-%   - channels:  eg { [1:30], [31:50], [51:100]}
-%   - TSindex:  one index to the ts in TS, eg 2
-
-global TS;
-nsplits = length(frames);
-newTSindices = tsNew(nsplits);
-for p=1:nsplits
-  TS{newTSindices(p)} = TS{TSindex};
-  TS{newTSindices(p)}.potvals = TS{newTSindices(p)}.potvals(:,frames{p});
-  TS{newTSindices(p)}.numframes = length(frames{p});
-end
-TS{TSindex}=1; % clear memory
-
-
-
-
-function frames = getFrames(TSindex)
-%frames = {[s1:e2],[s2:e2],...,[sn:en]}
-%frames shorter then 70 frames are removed
-
-
-global TS myScriptData
-numframes=TS{TSindex}.numframes;
-intervalLength=myScriptData.SAMPLEFREQ*myScriptData.SPLITINTERVAL;
-numIntervals=ceil(numframes/intervalLength);
-
-[frames{1:numIntervals}]=deal([]); % pre initialize
-for p=1:numIntervals-1
-    frames{p}=((p-1)*intervalLength+1):(p*intervalLength);
-end
-frames{end}=((numIntervals-1)*intervalLength+1):numframes;
-
-% remove frames that are too short (<70)
-idx2beRemoved=[];
-for p=1:length(frames)
-    if length(frames{p}) < 70
-        idx2beRemoved=[idx2beRemoved, p];
-    end
-end
-frames(idx2beRemoved)=[];
         
-
-
-
 
 
 
